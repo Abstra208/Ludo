7119e40b81f3a2083f11d3b30afdb72509ca36aa
Abstra208 <94720847+Abstra208@users.noreply.github.com>
2023-02-22 10:49:44 -0500

    fd

--- script.js
+++ script.js
@@ -8,45 +8,6 @@ Array.from(tabs).forEach(tab => {
         this.setAttribute("data-status", "active");
     });
 });
-//ADD TAB
-const addTabButton = document.getElementById("addTab");
-addTabButton.addEventListener("click", function() {
-    const tabs = document.querySelector(".tabs");
-    const newTab = document.createElement("button");
-    newTab.innerText = "Onglet " + (tabs.children.length + 1);
-    tabs.appendChild(newTab);
-    
-    Array.from(tabs.children).forEach(function(tab) {
-        tab.setAttribute("data-status", "inactive");
-
-    newTab.setAttribute("data-status", "active");
-
-    tab.addEventListener("click", function() {
-        Array.from(tabs.children).forEach(function(tab) {
-                tab.setAttribute("data-status", "inactive");
-            });
-            this.setAttribute("data-status", "active");
-        });
-    });
-});
-//REMOVE TAB
-const removeTabButton = document.getElementById("removeTab");
-removeTabButton.addEventListener("click", function() {
-    const tabs = document.querySelectorAll(".tabs button");
-    if (tabs.length < 2) {
-        return;
-    }
-
-    const activeTab = document.querySelector(".tabs button[data-status='active']");
-    const activeTabIndex = Array.from(tabs).indexOf(activeTab);
-    activeTab.remove();
-
-    if (activeTabIndex === tabs.length - 1 && activeTabIndex !== 0) {
-        tabs[activeTabIndex - 1].setAttribute("data-status", "active");
-    } else {
-        tabs[activeTabIndex === 0 ? 0 : activeTabIndex].setAttribute("data-status", "active");
-    }
-});
 //MENU SVG
 const menuIcon = document.querySelector('#menu-icon');
 const navtabs = document.querySelector(".tabs")
